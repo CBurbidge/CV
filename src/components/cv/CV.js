69f0c54c67d7f0cd1aa8457aae7d8be1bd098cf6
Chester Burbidge <chester.burbidge@gmail.com>
2018-03-19 23:39:54 +0000

    Added cv component

---
+++ src/components/cv/CV.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CV extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          CV, ohhhhhhh yeah.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CV;
