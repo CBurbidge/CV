47fabd72cdbc18e49be817563be06104a8546ea2
Chester Burbidge <chester.burbidge@gmail.com>
2018-08-06 19:47:31 +0100

    Added puppeteer

--- json/skills/skills.js
+++ json/skills/skills.js
@@ -660,6 +660,17 @@ module.exports = {
 			settings.abide,
 		]
 	),
+	
+	puppeteer: "puppeteer".toSkill(
+		"Puppeteer",
+		"Puppeteer is a Node library which provides a high-level API to control Chrome or Chromium over the DevTools Protocol. Puppeteer runs headless by default, but can be configured to run full (non-headless) Chrome or Chromium",
+		[
+			skillTypes.frontend,
+			skillTypes.testing,
+		],
+		[
+		]
+	),
 
 	python: "python".toSkill(
 		"python",
